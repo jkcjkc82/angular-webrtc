19a20dc325c0c1b2f20b85cb38d9145d3260f7ad
Kichul Jung <jung@Kichul-Jung-ui-iMac.local>
2013-06-20 00:22:39 +0900

    working...

--- angular-webrtc-module.js
+++ angular-webrtc-module.js
@@ -1,79 +1,13 @@
 angular.module('webrtc-module', []).
-factory('peerConnection2', function() {
-	return function() {
-		return {
-			create: function() {
-				var that = this;
-				// console.log("peerConnection.create()");
-				var servers = {
-					iceServers:[{
-						// url:"stun:50.112.159.100"
-						url:"stun:stun.l.google111.com:19302"						
-						// url: "stun:23.21.150.121"
-					}]
-				};	
-				// var pc_config = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};	
-				// {"iceServers":[{"url":"stun:23.21.150.121"}]}
-				var PeerConnection = typeof webkitRTCPeerConnection != "undefined" ? webkitRTCPeerConnection : mozRTCPeerConnection;				
-				var peerConnection = new PeerConnection(servers,{
-				  optional: [{'DtlsSrtpKeyAgreement': true}, {'RtpDataChannels': true }]
-				});
-
-				peerConnection.onaddstream = function(e) {
-					that.onaddtream(e);
-				};		
-				peerConnection.onremovestream = function() {
-					console.log("onremovestream");
-				};
-				peerConnection.ondatachannel = function() {
-					console.log("ondatachannel");
-				};	
-				peerConnection.onconnection = function() {
-					console.log("onconnection peer");
-				};			
-
-				this.peer = peerConnection;
-			},
-			candidate: function(callback) {
-				var that = this;
-				this.peer.onicecandidate = function(e) {
-					console.log("candidate start");
-					if(!e.candidate) {
-						return;
-					}
-
-					callback(e);
-				};
-			},
-			offer: function(callback) {
-				var that = this;
-				// console.log("peerConnection.offer()");
-				var offer = this.peer.createOffer(function(description) {					
-					console.log("1. offer - setLocalDescription",description);
-					that.peer.setLocalDescription(description);				
-
-					callback(description);
-				});	
-			},
-			answer: function(description,callback) {
-				var that = this;
-				// console.log("peerConnection.answer()");
-				// description.sdp = transformOutgoingSdp(description.sdp);
-				console.log("2. answer - setRemoteDescription",description);
-				this.peer.setRemoteDescription(description);
-
-				this.peer.createAnswer(function(description) {
-					callback(description);
-				});			
-			}
-		};
-	};
-}).factory('peerConnection', function() {	
+factory('peerConnection', function() {	
 	var default_parameter = {
+		onicecandidate: function(e) {},
 		onaddstream: function(e) {},
 		onremovestream: function(e) {},
 		ondatachannel: function(e) {},
 		onconnection: function(e) {},
+		offerCallback: function(description) {},
+		answerCallback: function(description) {},
 		servers: {
 			iceServers:[{
 				url:"stun:stun.l.google111.com:19302"						
@@ -85,15 +19,12 @@ factory('peerConnection2', function() {
 	};
 
 	return function(parameter) {
-		parameter = angular.extend(default_parameter, parameter);
+		parameter = angular.extend(angular.extend({}, default_parameter), parameter);
 
 		return {
 			peer: null,
+			isFinishSignaling: 0,
 			create: function() {
-				var that = this;
-				var servers = {
-
-				};
 				var PeerConnection = typeof webkitRTCPeerConnection != "undefined" ? webkitRTCPeerConnection : mozRTCPeerConnection;				
 				this.peer = new PeerConnection(parameter.servers, parameter.configuration);
 
@@ -101,52 +32,37 @@ factory('peerConnection2', function() {
 				this.peer.onremovestream = parameter.onremovestream;
 				this.peer.ondatachannel = parameter.ondatachannel;
 				this.peer.onconnection = parameter.onconnection;
+				this.peer.onicecandidate = parameter.onicecandidate;
+			},
+			offer: function(callback) {
+				this.peer.createOffer(function(description) {
+					parameter.offerCallback(description);
+				});
+			},
+			answer: function(description) {
+				this.peer.createAnswer(parameter.answerCallback);
+			},
+			setLocalDescription: function(description) {
+				this.peer.setLocalDescription(description);
+			},
+			setRemoteDescription: function(description) {
+				this.peer.setRemoteDescription(description);
+			},
+			addIceCandidate: function(candidate) {
+				this.peer.addIceCandidate(candidate);
+			},
+			addStream: function(stream) {
+				this.peer.addStream(stream);
+			},
+			finishSignaling: function() {
+				this.isFinishSignaling = 1;
 			}
 		}
 	}	
-}).factory('socket', function() {
-	return function() {
-		return {
-			peer: null,
-			socket: null,
-			create: function() {
-				// 소켓 연결
-				this.socket = io.connect('http://localhost:3000');
-				// this.socket = io.connect('http://localhost:3000');
-			},
-			onConnect: function(callback) {
-				var that = this;
-				this.socket.on('connect', function() {			
-					that.socket.emit("join");
-					callback();
-				});
-			},
-			onRes: function(callback) {
-				var that = this;
-				// 소켓 응답
-				this.socket.on("res",function(res) {
-					// console.log("onRes.callback()",res);
-					var type = 0;
-
-					if(res.candidate && typeof(res.candidate) != "undfined") {
-						type = 1;
-					} else if(res.description && typeof(res.description) != "undefined") {
-						if(res.description.type == "answer") {
-							type = 2;
-						} else if(res.description.type == "offer") {
-							type = 3;											
-						}					
-					}			
-
-					callback(type,res);
-				});		
-			}
-		};		
-	};
 }).factory('mediastream', function() {
 	var default_parameter = {
 		constraints: {
-			"audio": true, 
+			"audio": false, 
 			"video": true
 		},
 		successCallback: function(stream) {},
@@ -154,12 +70,12 @@ factory('peerConnection2', function() {
 	};
 
 	return function(parameter) {
-		parameter = angular.extend(default_parameter, parameter);
+		parameter = angular.extend(angular.extend({}, default_parameter), parameter);
 
 		return {
 			getUserMedia: function() {
 				navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-				navigator.getUserMedia(parameter.constraints, parameter.successCallback, parameter.errorCallback);	
+				navigator.getUserMedia(parameter.constraints, parameter.successCallback, parameter.errorCallback);
 			}
 		};
 	};
--- example/peerConnection/peerConnection.js
+++ example/peerConnection/peerConnection.js
@@ -1,14 +1,69 @@
 angular.module('peer-connection', ['webrtc-module']).config(function() {
 });
 
-function PeerConnectionCtrl($scope, peerConnection) {
+function PeerConnectionCtrl($scope, mediastream, peerConnection) {
 	$scope.init = function() {
-	};
+		$scope.v1 = document.querySelector("#v1");
+		$scope.v2 = document.querySelector("#v2");
 
-	$scope.create = function() {
-		$scope.pc1 = new peerConnection();
+		$scope.pc1 = peerConnection({
+			offerCallback: function(description) {
+				$scope.pc1.setLocalDescription(description);
+				$scope.pc2.setRemoteDescription(description);
+				
+				$scope.pc2.answer();
+			},
+			onicecandidate: function(e) {
+				if(!e.candidate) return;	
+				if(!$scope.pc1.isFinishSignaling) return;			
+		
+				$scope.pc2.addIceCandidate(e.candidate);
+			},
+			onaddstream: function(e) {
+				$scope.v1.src = window.URL.createObjectURL(e.stream);
+			}
+		});
+
+		$scope.pc2 = peerConnection({
+			answerCallback: function(description) {
+				$scope.pc2.setLocalDescription(description);
+				$scope.pc1.setRemoteDescription(description);
+
+				$scope.pc1.finishSignaling();
+				$scope.pc2.finishSignaling();
+			},
+			onicecandidate: function(e) {
+				if(!e.candidate) return;
+				if(!$scope.pc2.isFinishSignaling) return;
+
+				$scope.pc1.addIceCandidate(e.candidate);
+			},
+			onaddstream: function(e) {
+				$scope.v2.src = window.URL.createObjectURL(e.stream);
+			}
+		});		
+
+		$scope.m1 = mediastream({
+			successCallback: function(stream) {
+				$scope.pc2.addStream(stream);
+			}
+		});		
+
+		$scope.m2 = mediastream({
+			successCallback: function(stream) {
+				$scope.pc1.addStream(stream);
+			}
+		});						
+
+		$scope.m1.getUserMedia();
+		$scope.m2.getUserMedia();
 
 		$scope.pc1.create();
+		$scope.pc2.create();	
+	};
+
+	$scope.offer = function() {
+		$scope.pc1.offer();
 	};
 
 	$scope.init();
